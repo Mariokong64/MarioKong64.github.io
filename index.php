0a89f8d6381eb8348398963e014e101ae89daf67
Mario <103150952+Mariokong64@users.noreply.github.com>
2023-10-24 13:02:07 -0600

    Adding the index with corrections
    This is the new index

--- index.php
+++ index.php
@@ -28,17 +28,24 @@
 </head>
 
 <body data-spy="scroll" data-target="#navbarNav" data-offset="50">
+
   <!-- BARRA DE MENÚ -->
   <nav class="navbar navbar-expand-lg scroll">
+
     <div class="container">
+
       <a class="navbar-brand" href="#"> Portafolio </a>
 
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+
         <span class="navbar-toggler-icon"></span>
+
       </button>
 
       <div class="collapse navbar-collapse" id="navbarNav">
+
         <ul class="navbar-nav mx-auto">
+
           <li class="nav-item">
             <a href="#intro" class="nav-link smoothScroll">Inicio</a>
           </li>
@@ -63,34 +70,51 @@
         <div class="mt-lg-0 mt-3 mb-4 mb-lg-0">
           <a href="archivos/CV de Mario Alberto López.pdf" target="_blank" class="custom-btn btn">Descargar CV</a>
         </div>
+
       </div>
+
     </div>
+
   </nav>
 
   <!-- INICIO -->
   <section class="hero d-flex flex-column justify-content-center align-items-center" id="intro">
+
     <div class="container">
+
       <div class="row">
+
         <div class="mx-auto col-lg-5 col-md-5 col-10">
           <img src="images/mario1.png" class="img-fluid rounded-circle" alt="" />
         </div>
 
         <div class="d-flex flex-column justify-content-center align-items-center col-lg-7 col-md-7 col-12">
+
           <div class="hero-text">
+
             <h1 class="hero-title">👋 Mario Alberto López <br /></h1>
 
             <a href="archivos/CV de Mario Alberto López.pdf" target="_blank" class="email-link">Descargar CV</a>
+
           </div>
+
         </div>
+
       </div>
+
     </div>
+
   </section>
 
   <!-- SOBRE MÍ -->
   <section class="about section-padding" id="about">
+
     <div class="container">
+
       <div class="row">
+
         <div class="col-lg-6 col-md-6 col-12 text-justify">
+
           <h3 class="mb-4">Sobre mí</h3>
 
           <p>
@@ -109,6 +133,7 @@
           </p>
 
           <ul class="mt-4 mb-5 mb-lg-0 profile-list list-unstyled personal-data">
+
             <li><strong>Nombre:</strong> Mario Alberto López</li>
 
             <li><strong>Año de nacimiento:</strong> 19 de Septiembre 1991</li>
@@ -116,65 +141,90 @@
             <li><strong>Lugar de residencia:</strong> Queretaro, Qro.</li>
 
             <li><strong>Email:</strong> as.mario.152653@gmail.com</li>
+
           </ul>
+
         </div>
 
         <div class="col-lg-5 mx-auto col-md-6 col-12">
           <img src="images/yo-trabajando2.jpeg" class="about-image img-fluid" alt="Foto trabajando" />
         </div>
+
       </div>
+
     </div>
+
   </section>
 
   <!-- HABILIDADES -->
   <section class="testimonials section-padding" id="testimonials">
+
     <div class="container text-center">
+
       <div class="row">
+
         <div class="col-12">
           <h3 class="mb-5 text-center">Habilidades</h3>
         </div>
+
       </div>
 
       <div class="row font-cool">
+
         <div class="col">HTML</div>
         <div class="col">CSS</div>
         <div class="col">Bootstrap</div>
+
       </div>
 
       <div class="row row-cool font-cool">
+
         <div class="col">PHP</div>
         <div class="col">JavaScript</div>
         <div class="col">Ajax</div>
+
       </div>
 
       <div class="row row-cool font-cool">
+
         <div class="col">MySQL</div>
         <div class="col">SQL Server</div>
         <div class="col">GitHub</div>
+
       </div>
 
       <div class="row row-cool font-cool">
+
         <div class="col">Java</div>
         <div class="col">C++</div>
         <div class="col">C#</div>
+
       </div>
 
       <div class="row row-cool font-cool">
+
         <div class="col">UML</div>
         <div class="col">Office</div>
         <div class="col">Inglés avanzado</div>
+
       </div>
+
     </div>
+
   </section>
 
   <!-- FAQ -->
   <section class="faq section-padding">
+
     <div class="container">
+
       <div class="row">
+
         <div class="col-lg-12 col-12">
           <h3 class="mb-5">Proyectos</h3>
 
           <div class="projects-description text-justify">
+
             <p>
               En esta sección se muestran los proyectos que he realizado hasta
               la fecha. Para cada uno de ellos realicé un vídeo explicativo el
@@ -183,26 +233,36 @@
               la base de datos o la funcionalidad de la aplicación, con mucho
               gusto lo podemos hacer en una videollamada.
             </p>
+
           </div>
 
           <div class="accordion" id="accordionExample">
-            <!-- Sección del sistema del sistema de control de inventarios -->
 
+            <!-- Sección del sistema del sistema de control de inventarios -->
             <div class="card">
+
               <div class="card-header" id="headingOne">
+
                 <h2 class="mb-0">
+
                   <button class="btn btn-link d-flex justify-content-between" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                     <span>1.- Sistema para el control de activos fijos</span>
-                    <i class="fas fa-chevron-down"></i>
                     <!-- Icono de flecha hacia abajo -->
+                    <i class="fas fa-chevron-down"></i>
                   </button>
+
                 </h2>
+
               </div>
 
               <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+
                 <div class="card-body">
+
                   <div class="row row-cool font-cool">
+
                     <div class="col-sm-8 text-justify">
+
                       <p>
                         Este es un sistema que realicé para la empresa AGCO
                         Corporation con el objetivo de llevar un control de
@@ -216,15 +276,20 @@
                         en PDF, asignar activos a empleados, consultar
                         historial de movimientos, etc.
                       </p>
+
                     </div>
 
                     <div class="col-sm-4">
+
                       <div>
                         <img src="images/af_system.png" class="img-fluid img-cool" alt="af System" />
                       </div>
 
+                      <!-- Botón de YouTube -->
                       <div class="row row-yutu-button">
+
                         <a href="https://www.youtube.com/watch?v=jwsv_pD3G0s" target="_blank" class="col d-flex yutu-button">
+
                           <div class="col" style="width: 5%; text-align: right">
                             <img src="images/yutuicon.png" class="img-fluid yutu-icon" alt="icono de yutu" />
                           </div>
@@ -232,31 +297,68 @@
                           <div class="col-sm-8 d-flex align-items-center" style="width: 95%">
                             <p class="yutu-text">Ver video</p>
                           </div>
+
                         </a>
+
                       </div>
+                      <!-- Fin del botón de YouTube -->
+
+                      <!-- Botón de Github -->
+                      <div class="row row-yutu-button">
+
+                        <a href="" target="_blank" class="col d-flex git-button">
+
+                          <div class="col" style="width: 5%; text-align: right">
+                            <img src="images/githu-icon.png" class="img-fluid git-icon" alt="icono de yutu" />
+                          </div>
+
+                          <div class="col-sm-8 d-flex align-items-center" style="width: 95%">
+                            <p class="yutu-text">Ver en Git</p>
+                          </div>
+
+                        </a>
+
+                      </div>
+                      <!-- Fin del botón de Github -->
+
                     </div>
+
                   </div>
+
                 </div>
+
               </div>
+
             </div>
 
             <!-- Sección del sistema del sistema de la base del conocimiento -->
 
             <div class="card">
+
               <div class="card-header" id="headingTwo">
+
                 <h2 class="mb-0">
+
                   <button class="btn btn-link d-flex justify-content-between collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+
                     <span>2.- Sistema de almacenamiento de información</span>
-                    <i class="fas fa-chevron-down"></i>
                     <!-- Icono de flecha hacia abajo -->
+                    <i class="fas fa-chevron-down"></i>
+
                   </button>
+
                 </h2>
+
               </div>
 
               <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+
                 <div class="card-body">
+
                   <div class="row row-cool font-cool">
+
                     <div class="col-sm-8 text-justify">
+
                       <p>
                         Este es un sistema muy sencillo que me pidieron en la
                         misma empresa AGCO Corporation, el cual tiene como
@@ -270,16 +372,20 @@
                         y funciones de forma tal que se puedan reutilizar en
                         la mayor parte de los modulos del sistema.
                       </p>
+
                     </div>
 
                     <div class="col-sm-4">
+
                       <div>
                         <img src="images/knowledge_system.png" class="img-fluid img-cool" alt="Knowledge System" />
                       </div>
 
                       <!-- Botón de Yutu -->
                       <div class="row row-yutu-button">
+
                         <a href="https://www.youtube.com/watch?v=wZMLFCN3l-0" target="_blank" class="col d-flex yutu-button">
+
                           <div class="col" style="width: 5%; text-align: right">
                             <img src="images/yutuicon.png" class="img-fluid yutu-icon" alt="icono de yutu" />
                           </div>
@@ -287,25 +393,54 @@
                           <div class="col-sm-8 d-flex align-items-center" style="width: 95%">
                             <p class="yutu-text">Ver video</p>
                           </div>
+
                         </a>
+
                       </div>
+                      <!-- Fin del botón de Yutu -->
+
                       <!-- Botón de Github -->
+                      <div class="row row-yutu-button">
 
+                        <a href="" target="_blank" class="col d-flex git-button">
 
+                          <div class="col" style="width: 5%; text-align: right">
+                            <img src="images/githu-icon.png" class="img-fluid git-icon" alt="icono de yutu" />
+                          </div>
+
+                          <div class="col-sm-8 d-flex align-items-center" style="width: 95%">
+                            <p class="yutu-text">Ver en Git</p>
+                          </div>
+
+                        </a>
+
+                      </div>
                       <!-- Fin del botón de Github -->
+
                     </div>
+
                   </div>
+
                 </div>
+
               </div>
+
             </div>
+
           </div>
+
         </div>
+
       </div>
+
     </div>
+
   </section>
 
   <section class="contact section-padding pt-0" id="contact">
+
     <div class="container">
+
       <div>
         <h3 class="mb-5 text-center contact-tittle" style="padding-top: 50px">Contacto</h3>
       </div>
@@ -313,11 +448,13 @@
       <!-- Sección de contacto -->
 
       <div class="contact-secction">
+
         <div class="text-center" style="padding-bottom: 25px">
           <h6>Envíame un mensaje</h6>
         </div>
 
         <form method="post" class="contact-form webform">
+
           <div class="form-group d-flex flex-column-reverse">
             <input type="text" class="form-control" name="nombreEmisor" id="nombreEmisor" placeholder="Tu Nombre" />
             <label for="nombreEmisor" class="webform-label">Nombre</label>
@@ -336,13 +473,18 @@
           <div class="align-items-center">
             <button type="submit" class="form-control" id="submit-button" name="submit" style="margin: auto">Enviar</button>
           </div>
+
           <?php
           $mensaje = new Mensaje();
           $mensaje->enviarMensaje()
           ?>
+
         </form>
+
       </div>
+
     </div>
+
   </section>
 
   <!-- SCRIPTS -->
@@ -351,6 +493,7 @@
   <script src="js/smoothscroll.js"></script>
   <script src="js/owl.carousel.min.js"></script>
   <script src="js/custom.js"></script>
+
 </body>
 
 </html>
